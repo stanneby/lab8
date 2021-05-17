ac8bcfe009eb39e7aacfee0d9b0ffa23ee962d46
stas <stas.v.nebyshinets@gmail.com>
2021-05-18 01:56:24 +0300

    feat: add app carcas

---
+++ src/servlet/NewMessageServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import entity.ChatMessage;
+import entity.ChatUser;
+
+
+@WebServlet(name = "NewMessageServlet")
+public class NewMessageServlet extends ChatServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // По умолчанию используется кодировка ISO-8859. Так как мы
+        // передаѐмданныевкодировкеUTF-8//
+        // то необходимо установить соответствующую кодировкуHTTP-запроса
+        request.setCharacterEncoding("UTF-8");
+        // ИзвлечьизHTTP-запросапараметр'message'
+        String message = (String)request.getParameter("message");
+        // Если сообщение не пустое, то
+
+        if(message!=null&& !"".equals(message)) {
+            // По имени из сессии получить ссылку на объект ChatUse
+            ChatUser author = activeUsers.get((String) request.getSession().getAttribute("name"));
+
+
+            synchronized(messages) {
+                // Добавить всписок сообщений новое
+                messages.add(new ChatMessage(message, author, Calendar.getInstance().getTimeInMillis()));
+            }
+
+        }
+        // Перенаправитьпользователянастраницусформойсообщения
+        response.sendRedirect("/Laba8/compose_message.jsp");
+    }
+}
+
--- web/index.jsp
+++ web/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: stasv
-  Date: 5/17/2021
-  Time: 8:36 PM
+  User: 37529
+  Date: 20.04.2021
+  Time: 19:16
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
