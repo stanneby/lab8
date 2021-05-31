58924c2b1313611c2c91e48abbf64892e0d2c32a
stas <stas.v.nebyshinets@gmail.com>
2021-05-31 10:07:18 +0300

    feat: add censorship

--- src/servlet/NewMessageServlet.java
+++ src/servlet/NewMessageServlet.java
@@ -13,6 +13,7 @@ import entity.ChatUser;
 @WebServlet(name = "NewMessageServlet")
 public class NewMessageServlet extends ChatServlet {
     private static final long serialVersionUID = 1L;
+    private static final String wordsToCensor = "mat, nepotrebshina, Gendo Ikari";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // По умолчанию используется кодировка ISO-8859. Так как мы
@@ -24,6 +25,10 @@ public class NewMessageServlet extends ChatServlet {
         // Если сообщение не пустое, то
 
         if(message!=null&& !"".equals(message)) {
+            String[] arrayOfWordsToCensor = wordsToCensor.split(",\\s");
+            for(int i = 0; i < arrayOfWordsToCensor.length; i++){
+                message = message.replaceAll(arrayOfWordsToCensor[i], "*бип*");
+            }
             // По имени из сессии получить ссылку на объект ChatUse
             ChatUser author = activeUsers.get((String) request.getSession().getAttribute("name"));
 
